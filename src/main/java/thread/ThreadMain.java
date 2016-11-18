8e97c82fd0720adc1949d99fe98113d24fea8d6d
Liuh <liuhui620613>
2016-11-18 10:16:58 +0800

    多线程测试

--- src/main/java/thread/ThreadMain.java
+++ src/main/java/thread/ThreadMain.java
@@ -8,7 +8,7 @@ import java.util.concurrent.BlockingQueue;
  */
 public class ThreadMain {
     public static void main(String [] args){
-        for(int times = 0; times < 10; times++){
+        for(int times = 1; times < 10; times++){
             final BlockingQueue queue = new ArrayBlockingQueue(10);
             long time1 = System.currentTimeMillis();
             for(int i = 0; i <10000 ;i ++){
@@ -18,6 +18,7 @@ public class ThreadMain {
                 t2.start();
             }
             long time2 = System.currentTimeMillis();
+            System.out.println("第：          "+times+"次");
             System.out.println("循环耗时：    "+(time2-time1)+"ms");
         }
 
