88705ec1fcbe752ffbc0343433fa3f71ee5838b3
Liuh <liuh@joyowo.com>
2018-10-12 15:25:30 +0800

    newFixedThreadPool

--- src/main/java/thread/executor/NewFixedThreadPool.java
+++ src/main/java/thread/executor/NewFixedThreadPool.java
@@ -8,11 +8,20 @@ import java.util.concurrent.*;
 import static thread.Constant.SDF;
 
 /**
- * 有返回值的线程
+ * Java通过Executors提供四种线程池，分别为：
+ *
+ * newCachedThreadPool创建一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程。
+ * newFixedThreadPool 创建一个定长线程池，可控制线程最大并发数，超出的线程会在队列中等待。
+ * newScheduledThreadPool 创建一个定长线程池，支持定时及周期性任务执行。
+ * newSingleThreadExecutor 创建一个单线程化的线程池，它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行。
+ *
+ * 优点
+ * 重用存在的线程，减少对象创建、消亡的开销，性能佳。
+ * 可有效控制最大并发线程数，提高系统资源的使用率，同时避免过多资源竞争，避免堵塞。
+ * 提供定时执行、定期执行、单线程、并发数控制等功能。
  *
  * @author Liuh
  */
-@SuppressWarnings("unchecked")
 public class NewFixedThreadPool {
 
 
