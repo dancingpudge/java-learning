5d47210dcf1ec69a41beef868594ff635703e75c
liuhui <liuhui620613@outlook.com>
2024-04-12 10:58:59 +0800

    boolean排序

--- src/main/java/demo/lambda/SortTest.java
+++ src/main/java/demo/lambda/SortTest.java
@@ -2,6 +2,11 @@ package demo.lambda;
 
 import cn.hutool.core.util.RandomUtil;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @description: TODO
  * @author: LiuH
@@ -11,9 +16,17 @@ public class SortTest {
 
 
     public static void main(String[] args) {
+        List<CarPark> carParkList = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            System.out.println(RandomUtil.randomInt(30,270));
+            carParkList.add(new CarPark(RandomUtil.randomInt(30, 270) % 2 == 0));
+
         }
+
+        carParkList = carParkList.stream()
+                .sorted(Comparator.comparing(CarPark::getIsEnable).reversed())
+                .collect(Collectors.toList());
+
+        carParkList.forEach(c -> System.out.println(c.getIsEnable()));
     }
 
     static class CarPark {
